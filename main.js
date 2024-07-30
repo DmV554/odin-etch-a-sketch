bcbc8774b096869fbfadbcff0a3cb666c5f2709f
DmV554 <daniel3318bl@gmail.com>
2024-07-30 01:09:24 -0400

    Complete proyect

--- main.js
+++ main.js
@@ -1,14 +1,67 @@
 const supremeContainer = document.querySelector(".container")
 
-for(i = 0; i<16; i++) {
-    const squareRowContainer = document.createElement("div")
-    squareRowContainer.classList.add("contenedor-cuadrado")
-    for(j=0; j<16; j++) {
-        const square = document.createElement("div")
-        square.textContent = "el pepe"
-        square.classList.add("cuadrado");
-
-        squareRowContainer.appendChild(square)
-    }
-    supremeContainer.appendChild(squareRowContainer)
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
 }
+
+function generarPizarra(size) {
+
+    const squareSize = 600 / size
+
+    for(i = 0; i<size; i++) {
+        const squareRowContainer = document.createElement("div")
+        squareRowContainer.classList.add("contenedor-cuadrado")
+        for(j=0; j<size; j++) {
+            const square = document.createElement("div")
+            square.classList.add("cuadrado");
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+    
+            square.addEventListener('mouseenter', () => {
+                if (!square.dataset.color) {
+                    const randomColor = getRandomColor();
+                    square.style.backgroundColor = randomColor;
+                    square.dataset.color = randomColor;
+                    square.style.opacity = 0.0;
+                }
+        
+                const currentOpacity = parseFloat(square.style.opacity);
+                if(currentOpacity < 1) {
+                    square.style.opacity = (currentOpacity + 0.1).toString();
+                }
+                
+            });
+            
+    
+            squareRowContainer.appendChild(square)
+        }
+        supremeContainer.appendChild(squareRowContainer)
+    }
+}
+
+
+generarPizarra(16)
+
+const boton = document.querySelector(".boton")
+
+function cambiarPizarra() {
+    const newSize = prompt("Especifica nuevo tamaño para la pizarra")
+
+    if(newSize > 100) {
+        alert("Tamaño no valido")
+        return
+    }
+
+    supremeContainer.replaceChildren()
+
+    generarPizarra(newSize)
+
+
+}
+
+boton.addEventListener("click", () => {
+    cambiarPizarra()
+})
